d57eee7813913f038d7c5108f917d6119eb8235d
matthew.serre <matthew.serre@gmail.com>
2018-09-17 01:36:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,5 +63,7 @@ function placeOrder(cardNumber) {
   // write your code here
   if (cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.")
+  } else {
+    return "Your total cost is " + total() + ", which will be charged to the card ${cardNumber}."
   }
 }
