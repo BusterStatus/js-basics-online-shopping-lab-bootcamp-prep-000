56ed29069cfd291c4f689458fc17263c1ba34a64
matthew.serre <matthew.serre@gmail.com>
2018-09-17 01:30:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function removeFromCart(item) {
   // write your code here
   for (i = 0; i < cart.length; i++) {
     if (cart[i][itemName] === item) {
-      delete cart[i];
+      cart.splice(cart[i], 1);
     }
   }
   return cart;
